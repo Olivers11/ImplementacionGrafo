74e6f912c10fda3f049e88ef2118ed6c7062b31a
Olivers11 <olstertecn597@outlook.com>
2022-11-18 02:05:54 -0600

    last changes

---
+++ ImplementacionGrafo/Celdas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementacionGrafo
+{
+    public class Celdas
+    {
+        public string id;
+        public List<int> referencias;
+        public List<int> posiciones;
+        public Celdas(string id)
+        {
+            this.id = id;
+            this.referencias = new List<int>();
+            this.posiciones = new List<int>();
+        }
+    }
+    public class Tabla
+    {
+        public List<Celdas> celdas;
+        public Tabla()
+        {
+            this.celdas = new List<Celdas>();
+        }
+    }
+}
---
+++ ImplementacionGrafo/Clausura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementacionGrafo
+{
+    public class Clausura
+    {
+        public int estado;
+        public List<int> posiciones;
+        public Clausura(int estado)
+        {
+            this.estado = estado;
+            this.posiciones = new List<int>();
+        }   
+    }
+}
